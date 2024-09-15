3c4d8c253e36439c10fceb76ed34677a517de501
Ramin Karimpor <ramin.kp81@gmail.com>
2024-09-15 22:03:13 +0330

    devloping home page

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,13 @@ import styles from "@/styles/home.module.scss";
 
 function index() {
   return (
-    <>
-      <h1>رامین کریم پور</h1>
-    </>
+    <main className={styles.home}>
+      <h1 className={styles.home__title}>
+        به <span>Next Todo</span> خوش آمدید لطفا برای استفاده از امکانات سایت
+        ثبت نام یا به حساب کاربری خود وارد شوید
+      </h1>
+      <div className={styles.header__login}>ورود | ثبت نام</div>
+    </main>
   );
 }
 
